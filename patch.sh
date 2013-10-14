3818c8bd5c774f10fec1496af1393ea410f14785
Nguyen Thanh Trung <nguyenthanh.trung@nomovok.vn>
2013-10-14 15:07:22 +0700

    added patch scripts

---
+++ patch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo =====patches====
+export REPO=$PWD/repo
+export B2G_TREEID_SH="$PWD/patches/treeid.sh"
+export B2G_HASHED_FILES="$PWD/patches/vendorsetup.sh ${B2G_TREEID_SH} $PWD/.config $PWD/patches/patch.sh"
+export B2G_PATCH_DIRS_OVERRIDE=patches
+
+. patches/vendorsetup.sh
